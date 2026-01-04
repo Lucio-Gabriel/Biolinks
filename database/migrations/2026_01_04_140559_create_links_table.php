14b247d719dbdbf825176525b5561d9838cd8862
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2026-01-04 12:14:13 -0300

    feat: create relation and links and user

--- database/migrations/2026_01_04_140559_create_links_table.php
+++ database/migrations/2026_01_04_140559_create_links_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
             $table->id();
             $table->string('link');
             $table->string('name');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
