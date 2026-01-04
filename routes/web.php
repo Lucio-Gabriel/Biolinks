621604a9d71d3895d332578bb6f269e5c3183018
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2026-01-04 11:24:29 -0300

    feat: create method of create link

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,11 +13,16 @@ Route::get('/', function () {
 Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'index'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
+
     Route::get('/register', [RegisterController::class, 'index'])->name('register');
     Route::post('/register', [RegisterController::class, 'register']);
 });
 
-Route::get('/logout', LogoutController::class)->middleware('auth')->name('logout');
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', LogoutController::class)->name('logout');
+    
+    Route::get('/dashboard', fn() => 'dashboard ::'. auth()->id())->name('dashboard');
 
-Route::get('/dashboard', fn() => 'dashboard ::'. auth()->id())
-    ->middleware('auth')->name('dashboard');
+    Route::get('/links/create', [LinkController::class, 'create'])->name('links.create');
+    Route::post('/links/create', [LinkController::class, 'store']);
+});
