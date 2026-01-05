7fbca191f8d9484f6f0d4c01cf0e2046a1b3a888
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2026-01-05 08:09:54 -0300

    feat: create list of links

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 12345678
+        $this->call([
+            UserSeeder::class,
+            LinkSeeder::class,
         ]);
     }
 }
