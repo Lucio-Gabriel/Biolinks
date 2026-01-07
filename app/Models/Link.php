c00e3bd13fdd6a0fe8f4d0cdcd6d3f1ebe319a66
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2026-01-07 15:32:19 -0300

    feat: update method of move links and controller and model

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -7,6 +7,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
-    /** @use HasFactory<\Database\Factories\LinkFactory> */
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function moveUp()
+    {
+        $this->move(-1);
+    }
+
+    public function moveDown()
+    {
+        $this->move(+1);
+    }
+
+    /**
+     * Function responsible for reorder the link
+     * 
+     * @param int $to
+     * @return void
+     */
+
+    private function move($to)
+    {
+        $order = $this->sort;
+        $newOrder = $order + $to;
+
+        $swapWith = $this->user->links()->where('sort', '=', $newOrder)->first();
+
+        $this->fill(['sort' => $newOrder])->save();
+        $swapWith->fill(['sort' => $order])->save();
+    }
 }
